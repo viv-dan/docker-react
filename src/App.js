0337b57e870c4cb01adb286bee0d1752242bd9c0
viv-dan <81843793+viv-dan@users.noreply.github.com>
2023-08-26 13:37:40 -0400

    Revert "pushing to feature"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	Hi There! How are you? from feature
+	Hi There! How are you?
         </p>
         <a
           className="App-link"
